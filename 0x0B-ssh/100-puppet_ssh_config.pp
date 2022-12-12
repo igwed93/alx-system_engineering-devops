743e3e1245c386e4e4b42b84814fb1aaf4fbde48
Daniel O. Igwe <105175646+igwed93@users.noreply.github.com>
2022-12-12 03:48:40 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,8 +3,19 @@
 # configure ssh client to use private key and refuse
 # password authentication
 
-Host 50998-web-01
-    HostName 54.157.182.63
-    User    ubuntu
-    IdentityFile    ~/.ssh/school
-    PasswordAuthentication  no
+include stdlib
+file_line { 'NO password':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    PasswordAuthentication no',
+    match              => '^PasswordAuthentication',
+    append_on_no_match => true,
+  }
+
+file_line { 'New private key holder':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    IdentityFile ~/.ssh/school',
+    match              => '^IdentityFile',
+    append_on_no_match => true,
+  }
