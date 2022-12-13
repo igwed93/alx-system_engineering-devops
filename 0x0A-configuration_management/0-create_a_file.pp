56283ac6b3f65090d4b74682abcc8968bf72ab9e
Daniel Igwe <daniel.igwe000@gmail.com>
2022-12-13 11:34:42 +0100

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Using puppet, create a file with the specifications below.
+# Using puppet, create a file with below specifications
 
 file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
-  mode    => '0744'
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
