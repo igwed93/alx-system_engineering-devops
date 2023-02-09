682c0aab088a4698c51f9a921d2ce46d7027500d
Daniel Igwe <daniel.igwe000@gmail.com>
2023-02-09 14:51:34 +0100

    strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Apache return 500 error
+# fixes type in config
+
+exec {'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
